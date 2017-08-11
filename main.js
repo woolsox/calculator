4447d4ae70137d0ca124b6e32bc42aacdc7b8737
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-11 08:05:25 -0400

    restyled the whole calc, added mem, sqrt, and parentheses buttons

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
+// global variables
+
 let display = document.querySelector('.display_value');
 let displayEval = document.querySelector('.display_eval');
 let evaluation = [];
-let answer;
+let lastEval = [];
 
 // integer/operator input buttons
 
-let numberInput = document.querySelectorAll('.input');
+let input = document.querySelectorAll('.input');
 
-for (let i = 0; i < numberInput.length; i++) {
-   numberInput[i].addEventListener('click', function(){
+for (let i = 0; i < input.length; i++) {
+   input[i].addEventListener('click', function(){
       evaluation += this.value;
       display.innerHTML = evaluation;
-      console.log(evaluation);
-      console.log(evaluation.length);
    });
 }
+
+// square root button
+
+let sqRoot = document.querySelector('.sqRoot');
+
+sqRoot.addEventListener('click', function(){
+   let answer = (eval(evaluation));
+   displayEval.innerHTML = Math.sqrt(answer);
+   lastEval.push(display.innerHTML);
+   lastEval.push(displayEval.innerHTML);
+});
 
 // evalute button
 
@@ -22,8 +33,9 @@ let evaluate = document.querySelector('.evaluate');
 
 evaluate.addEventListener('click', function(){
    let answer = (eval(evaluation));
-   displayEval.innerHTML = '= ' + answer;
-   console.log(eval(evaluation));
+   displayEval.innerHTML = answer.toLocaleString({minimumFractionDigits: 2});;
+   lastEval.push(display.innerHTML);
+   lastEval.push(displayEval.innerHTML)
 });
 
 // clear button
@@ -34,7 +46,14 @@ clear.addEventListener('click', function(){
    evaluation = [];
    display.innerHTML = evaluation;
    displayEval.innerHTML = evaluation;
-   console.log(evaluation);
 });
 
-// number check
+// memory button
+
+let memory = document.querySelector('.memory');
+
+memory.addEventListener('click', function() {
+   display.innerHTML = lastEval[0];
+   displayEval.innerHTML = lastEval[1];
+   lastEval = [];
+});
